de86c55cf7e52c14668d2ba3cd9dbe910a3581a4
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-10-19 20:27:35 +0800

    Disable Angular SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -9,12 +8,12 @@ import { LoadSpeakersService } from './load-speakers.service';
 import { LoadScheduleService } from './load-schedule.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(withFetch()), {
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch()), {
     provide: APP_INITIALIZER,
     useFactory: (service: LoadSpeakersService) => () => service.load(),
     deps: [LoadSpeakersService],
     multi: true
-  },{
+  }, {
     provide: APP_INITIALIZER,
     useFactory: (service: LoadScheduleService) => () => service.load(),
     deps: [LoadScheduleService],

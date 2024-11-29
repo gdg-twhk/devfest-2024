ef8f7c58273331df6228fbea1c0c9b3e2168c3ee
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-11-29 23:02:39 +0800

    feat: add sponsor list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Sponsor {
+  name: string;
+  src: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  sponsors: Sponsor[] = [
+    {
+      src: './assets/sponsors/adecco.png',
+      name: '藝珂集團',
+      link: 'https://www.adecco.com.tw/',
+    },
+    {
+      src: './assets/sponsors/apmic.png',
+      name: '亞太智能機器',
+      link: 'https://www.ap-mic.com/',
+    },
+    {
+      src: './assets/sponsors/天瓏書局.jpg',
+      name: '天瓏書局',
+      link: 'https://www.tenlong.com.tw/',
+    },
+    {
+      src: './assets/sponsors/jetbrains.png',
+      name: 'JetBrains',
+      link: 'https://www.jetbrains.com/',
+    },
+    {
+      src: './assets/sponsors/TaipeiComputerAssociation.svg',
+      name: '台北市電腦商業同業公會',
+      link: 'https://www.tca.org.tw/',
+    },
+  ];
+}
